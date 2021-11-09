84db15f7249826ffefec566e6a58748fb0909d1b
woobeen <drogba02@naver.com>
2021-11-09 17:05:13 +0900

    add - annotation jar file 패키지 생성 , 클래스 생성코드 작성

--- annotation-processor-example/magicmoja/src/main/java/MagicMojaProcessor.java
+++ annotation-processor-example/magicmoja/src/main/java/MagicMojaProcessor.java
@@ -1,13 +1,20 @@
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import java.io.IOException;
 import java.util.Set;
 
 @AutoService(Processor.class)
@@ -30,6 +37,31 @@ public class MagicMojaProcessor extends AbstractProcessor {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR , "Magic annotation can not be used on " +  element.getSimpleName());
             } else {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE , "Processing " + element.getSimpleName());
+            }
+
+            TypeElement typeElement = (TypeElement) element;
+            ClassName className = ClassName.get(typeElement);
+
+            MethodSpec pullOut = MethodSpec.methodBuilder("pullOut")
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(String.class)
+                    .addStatement("return $S" , "Rabbit!")
+                    .build();
+
+            TypeSpec magicMoja = TypeSpec.classBuilder("MagicMoja")
+                    .addModifiers(Modifier.PUBLIC)
+                    .addSuperinterface(className)
+                    .addMethod(pullOut)
+                    .build();
+
+            Filer filer = processingEnv.getFiler();
+
+            try {
+                JavaFile.builder(className.packageName() , magicMoja)
+                        .build()
+                        .writeTo(filer);
+            } catch (IOException e) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR , "FATAL ERROR " + e);
             }
         }
 
