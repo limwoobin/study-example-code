6b7a76187a2ea05bd328c5f0e104a90b66337bce
limwoobin <drogba02@naver.com>
2021-12-04 19:51:46 +0900

    spring runner - Bean 구성정보 조합

---
+++ spring-runner/learn-essences-of-spring-template/src/test/java/moviebuddy/domain/BeanScopeTests.java
@@ -0,0 +1,19 @@
+package moviebuddy.domain;
+
+import moviebuddy.MovieBuddyFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanScopeTests {
+
+    @Test
+    void Equals_MovieFinderBean() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
+        MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
+        MovieFinder movieFinder2 = applicationContext.getBean(MovieFinder.class);
+
+        Assertions.assertEquals(movieFinder, movieFinder2);
+    }
+}
