ffeb7e6f15407519c87f8ab43cf23e1d6afe7c0a
limwoobin <drogba02@naver.com>
2021-12-04 19:51:46 +0900

    spring runner - Bean 구성정보 조합

--- spring-runner/learn-essences-of-spring-template/src/main/java/moviebuddy/MovieBuddyFactory.java
+++ spring-runner/learn-essences-of-spring-template/src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -2,14 +2,28 @@ package moviebuddy;
 
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
+import moviebuddy.domain.MovieReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({ MovieBuddyFactory.DataSourceModuleConfig.class , MovieBuddyFactory.DataSourceModuleConfig.class })
 public class MovieBuddyFactory {
 
-    @Bean
-    public MovieFinder movieFinder() {
-        return new MovieFinder(new CsvMovieReader());
+    @Configuration
+    static class DomainModuleConfig {
+        @Bean
+        public MovieFinder movieFinder(MovieReader movieReader) {
+            return new MovieFinder(movieReader);
+        }
+    }
+
+    @Configuration
+    static class DataSourceModuleConfig {
+        @Bean
+        public MovieReader movieReader() {
+            return new CsvMovieReader();
+        }
     }
 }
