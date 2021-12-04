e4ca50db755bf00b6df1f5c79773d90f5c34ef38
limwoobin <drogba02@naver.com>
2021-12-04 20:57:05 +0900

    spring runner - 스프링 통합 테스트 연결

--- spring-runner/learn-essences-of-spring-template/src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ spring-runner/learn-essences-of-spring-template/src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -3,18 +3,23 @@ package moviebuddy.domain;
 import moviebuddy.MovieBuddyFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 
 /**
  * @author springrunner.kr@gmail.com
  */
+//@SpringJUnitConfig(MovieBuddyFactory.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = MovieBuddyFactory.class)
 public class MovieFinderTest {
-	final ApplicationContext applicationContext =
-			new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
-	final MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
+	@Autowired
+	MovieFinder movieFinder;
 
 	@Test
 	void NotEmpty_directedBy() {
