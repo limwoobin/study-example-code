03d5a25e0212c577da2ff0e80a0148a52da5cde6
limwoobin <drogba02@naver.com>
2021-12-05 15:11:26 +0900

    spring runner - Xml Reader 추상화

--- spring-runner/learn-essences-of-spring-template/src/main/java/moviebuddy/domain/MovieFinder.java
+++ spring-runner/learn-essences-of-spring-template/src/main/java/moviebuddy/domain/MovieFinder.java
@@ -1,7 +1,6 @@
 package moviebuddy.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
