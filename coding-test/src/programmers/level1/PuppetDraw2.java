86ee443ff3c954047a6620745d5b5668119004a3
limwoobin <drogba02@naver.com>
2022-03-01 14:32:41 +0900

    level1 - 인형뽑기 (copy)

---
+++ coding-test/src/programmers/level1/PuppetDraw2.java
@@ -0,0 +1,31 @@
+package programmers.level1;
+
+import java.util.Stack;
+
+public class PuppetDraw2 {
+    public static void main(String[] args) {
+        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[] moves = {1,5,3,5,1,2,1,4};
+        int cnt = 0;
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        for (int move : moves) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][move - 1] != 0) {
+                    if (stack.peek() == board[j][move - 1]) {
+                        stack.pop();
+                        cnt += 2;
+                    } else {
+                        stack.push(board[j][move - 1]);
+                    }
+                    board[j][move - 1] = 0;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
