30f7ee02d0bbaa5348840c0052ef976e99a381fd
limwoobin <drogba02@naver.com>
2022-03-05 18:06:48 +0900

    level1 - 없는 숫자 누르기

---
+++ coding-test/src/programmers/level1/NotExistsNumberSum.java
@@ -0,0 +1,28 @@
+package programmers.level1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+* 없는 숫자 누르기
+* https://programmers.co.kr/learn/courses/30/lessons/86051
+* */
+public class NotExistsNumberSum {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i=0; i<10; i++) {
+            list.add(i);
+        }
+
+        int numbers[] = {1,2,3,4,6,7,8,0};
+        for (int num : numbers) {
+            list.remove(Integer.valueOf(num));
+        }
+
+        int sum = list.stream()
+                        .mapToInt(x -> x)
+                        .sum();
+
+        System.out.println(sum);
+    }
+}
