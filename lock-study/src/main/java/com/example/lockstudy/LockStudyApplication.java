46a29dfbc460f265c23c13c4538f58befca39535
limwoobin <drogba02@naver.com>
2022-08-29 19:53:22 +0900

    lock study project init

---
+++ lock-study/src/main/java/com/example/lockstudy/LockStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.lockstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockStudyApplication.class, args);
+    }
+
+}
---
+++ lock-study/src/test/java/com/example/lockstudy/LockStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lockstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
