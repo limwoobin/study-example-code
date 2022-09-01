3d1077df46b1b86d607777645e117f318cf4ea41
limwoobin <drogba02@naver.com>
2022-09-01 19:35:07 +0900

    feat: Redis의 Redisson lib 락을 이용한 동시성 예제

---
+++ lock-study/src/main/java/com/example/lockstudy/facade/RedissonLockStockFacade.java
@@ -0,0 +1,34 @@
+package com.example.lockstudy.facade;
+
+import com.example.lockstudy.service.StockService;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedissonLockStockFacade {
+    private final RedissonClient redissonClient;
+    private final StockService stockService;
+
+    public void decrease(Long key, Long quantity) {
+        RLock lock = redissonClient.getLock(key.toString());
+
+        try {
+            boolean available = lock.tryLock(5, 3, TimeUnit.SECONDS);
+
+            if (!available) {
+                System.out.println("Lock 획득 실패");
+                return;
+            }
+
+            stockService.decrease(key, quantity);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
---
+++ lock-study/src/test/java/com/example/lockstudy/facade/RedissonLockStockFacadeTest.java
@@ -0,0 +1,57 @@
+package com.example.lockstudy.facade;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.lockstudy.domain.Stock;
+import com.example.lockstudy.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedissonLockStockFacadeTest {
+
+    @Autowired
+    private RedissonLockStockFacade redissonLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    void setUp() {
+        Stock stock = new Stock(1L, 100L);
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    void teardown() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 동시에_100개_요청_redisson_lock() throws InterruptedException {
+        int numberOfThreads = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+
+        for (int i=0; i<numberOfThreads; i++) {
+            executorService.submit(() -> {
+                try {
+                    redissonLockStockFacade.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(0, stock.getQuantity());
+    }
+}
