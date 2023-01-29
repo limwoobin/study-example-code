c837215d876d3f4d38ca7b2035158000eb896955
woobeen <drogba02@naver.com>
2023-01-29 16:52:18 +0900

    refactor: kafka consumer errorhandler retry, recoveryCallback 설정 추가

--- spring-kafka-consumer/src/main/java/com/example/springkafkaconsumer/consumer/config/KafkaConsumerConfig.java
+++ spring-kafka-consumer/src/main/java/com/example/springkafkaconsumer/consumer/config/KafkaConsumerConfig.java
@@ -3,7 +3,9 @@ package com.example.springkafkaconsumer.consumer.config;
 import com.example.springkafkaconsumer.consumer.application.TestVO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,18 +15,25 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.converter.RecordMessageConverter;
 import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.util.backoff.BackOff;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 @EnableKafka
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaConsumerConfig {
 
     @Value("${spring.kafka.consumer.bootstrap-servers}")
@@ -33,7 +42,17 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.consumer.group-id}")
     private String groupId;
 
+    @Value(value = "${spring.kafka.backoff.interval}")
+    private Long interval;
+
+    @Value(value = "${spring.kafka.backoff.max_failure}")
+    private Long maxAttempts;
+
+    @Value("${spring.kafka.topics.dead-letter}")
+    private String deadLetter;
+
     private final ObjectMapper objectMapper;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
@@ -59,6 +78,7 @@ public class KafkaConsumerConfig {
 
         factory.setConsumerFactory(consumerFactory());
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        factory.setCommonErrorHandler(errorHandler());
 
         return factory;
     }
@@ -101,5 +121,28 @@ public class KafkaConsumerConfig {
     @Bean
     public RecordMessageConverter converter(){
         return new StringJsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public DefaultErrorHandler errorHandler() {
+        BackOff fixedBackOff = new FixedBackOff(interval, maxAttempts);
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler((consumerRecord, exception) -> {
+            ConsumerRecord<String, String> record = (ConsumerRecord<String, String>) consumerRecord;
+            log.error("[KafkaErrorHandler] {}, {}, {}, {}, {}",
+                    kv("topic", record.topic()),
+                    kv("offset", record.offset()),
+                    kv("value", record.value()),
+                    kv("exception", exception.getCause()),
+                    kv("exceptionMessage", exception.getCause().getMessage())
+            );
+
+            kafkaTemplate.send(deadLetter, record.value());
+            log.info("dead letter send success {}", consumerRecord.value());
+        }, fixedBackOff);
+
+//        errorHandler.addRetryableExceptions(SocketTimeoutException.class);    // 재시도 할 수 있는 예외
+//        errorHandler.addNotRetryableExceptions(NullPointerException.class);   // 재시도 할 수 없는 예외
+
+        return errorHandler;
     }
 }
