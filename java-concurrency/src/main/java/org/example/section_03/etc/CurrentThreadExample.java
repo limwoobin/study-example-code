f208007047e7bc99cf1c27ab9909c5279b12503e
limwoobin <drogba02@naver.com>
2024-05-05 22:38:53 +0900

    feat: Java Concurrency Project Section03 priority

---
+++ java-concurrency/src/main/java/org/example/section_03/etc/CurrentThreadExample.java
@@ -0,0 +1,30 @@
+package org.example.section_03.etc;
+
+public class CurrentThreadExample {
+  public static void main(String[] args) {
+    System.out.println("현재 스레드(main): " + Thread.currentThread());
+    System.out.println("현재 스레드 이름(main): " + Thread.currentThread().getName());
+
+    Thread thread = new Thread() {
+      @Override
+      public void run() {
+        System.out.println("현재 스레드(thread): " + Thread.currentThread());
+        System.out.println("현재 스레드(thread): " + getName());
+      }
+    };
+
+    thread.start();
+
+    Thread thread1 = new Thread(new ThreadName());
+    thread1.start();
+  }
+
+  static class ThreadName implements Runnable {
+
+    @Override
+    public void run() {
+      System.out.println("현재 스레드(Runnable 사용): " + Thread.currentThread());
+      System.out.println("현재 스레드(Runnable 사용): " + Thread.currentThread().getName());
+    }
+  }
+}
---
+++ java-concurrency/src/main/java/org/example/section_03/etc/ThreadNamingExample.java
@@ -0,0 +1,26 @@
+package org.example.section_03.etc;
+
+public class ThreadNamingExample {
+  public static void main(String[] args) throws InterruptedException {
+    Thread thread = new Thread(() -> {
+      System.out.println("현재 스레드 이름: " + Thread.currentThread().getName());
+    }, "myThread");
+    thread.start();
+
+    Thread otherThread = new Thread(() -> {
+      System.out.println("현재 스레드 이름: " + Thread.currentThread().getName());
+    });
+    otherThread.setName("otherThread");
+    otherThread.start();
+
+    for (int i = 0; i < 5; i++) {
+      Thread basicThread = new Thread(() -> {
+        System.out.println("현재 스레드 이름: " + Thread.currentThread().getName());
+      });
+
+      basicThread.start();
+    }
+
+    Thread.sleep(2000);
+  }
+}
