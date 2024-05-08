cdda3dc992d3c2ef991c17e38081d94642490543
limwoobin <drogba02@naver.com>
2024-05-08 21:33:41 +0900

    feat: Java Concurrency Project Section05 Safety Thread

---
+++ java-concurrency/src/main/java/org/example/section_05/exam03/NonRaceConditionExample.java
@@ -0,0 +1,28 @@
+package org.example.section_05.exam03;
+
+public class NonRaceConditionExample {
+  private static int sharedResource = 0;
+
+  public static void main(String[] args) throws InterruptedException {
+    // 스레드 100개를 생성하여 공유 리소스를 동시에 증가시킴
+    Thread[] incrementThreads = new Thread[100];
+    for (int i = 0; i < incrementThreads.length; i++) {
+      incrementThreads[i] = new Thread(() -> {
+        for (int j = 0; j < 10000; j++) {
+          synchronized(NonRaceConditionExample.class) {
+            sharedResource++; // 각 스레드가 공유 데이터에 동시에 접근할 수 없다
+          }
+        }
+      });
+      incrementThreads[i].start();
+    }
+
+    // 모든 스레드가 작업을 완료할 때까지 기다림
+    for (int i = 0; i < incrementThreads.length; i++) {
+      incrementThreads[i].join();
+    }
+
+    System.out.println("Expected value = " + (100 * 10000));
+    System.out.println("Actual value = " + sharedResource);
+  }
+}
---
+++ java-concurrency/src/main/java/org/example/section_05/exam03/RaceConditionExample.java
@@ -0,0 +1,26 @@
+package org.example.section_05.exam03;
+
+public class RaceConditionExample {
+  private static int sharedResource = 0;
+  public static void main(String[] args) throws InterruptedException {
+    Thread[] incrementThreads = new Thread[100];
+    for (int i = 0; i < incrementThreads.length; i++) {
+      incrementThreads[i] = new Thread(() -> {
+        for (int j = 0; j < 10000; j++) {
+          synchronized(NonRaceConditionExample.class) {
+            sharedResource++; // 각 스레드가 공유 데이터에 동시에 접근할 수 없다
+          }
+        }
+      });
+      incrementThreads[i].start();
+    }
+
+    // 모든 스레드가 작업을 완료할 때까지 기다림
+    for (int i = 0; i < incrementThreads.length; i++) {
+      incrementThreads[i].join();
+    }
+
+    System.out.println("Expected value = " + (100 * 10000));
+    System.out.println("Actual value = " + sharedResource);
+  }
+}
