517537b0ed9536d3c4733cc79e3a2c00aef0364f
limwoobin <drogba02@naver.com>
2024-05-11 21:11:55 +0900

    feat: Java Concurrency Project Section06 Monitor, SpinLock

---
+++ java-concurrency/src/main/java/org/example/section_06/exam03/ConditionSynchronizationExample.java
@@ -0,0 +1,52 @@
+package org.example.section_06.exam03;
+
+public class ConditionSynchronizationExample {
+  private boolean isAvailable = false;
+
+  public synchronized void produce() {
+    while (isAvailable) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    System.out.println("생산됨");
+    this.isAvailable = true;
+    notify();
+  }
+
+  public synchronized void consume() {
+    while (!isAvailable) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    System.out.println("소비됨");
+    this.isAvailable = false;
+    notify();
+  }
+
+  public static void main(String[] args) {
+    ConditionSynchronizationExample example = new ConditionSynchronizationExample();
+
+    Thread thread = new Thread(() -> {
+      for (int i = 0; i < 5; i++) {
+        example.produce();
+      }
+    });
+
+    Thread thread2 = new Thread(() -> {
+      for (int i = 0; i < 5; i++) {
+        example.consume();
+      }
+    });
+
+    thread.start();
+    thread2.start();
+  }
+}
---
+++ java-concurrency/src/main/java/org/example/section_06/exam04/SpinLockExample.java
@@ -0,0 +1,40 @@
+package org.example.section_06.exam04;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class SpinLockExample {
+
+  private AtomicBoolean lock = new AtomicBoolean(false);
+
+  public void lock() {
+    while (!lock.compareAndSet(false, true));
+  }
+
+  public void unLock() {
+    lock.set(false);
+  }
+
+  public static void main(String[] args) {
+    SpinLockExample spinLock = new SpinLockExample();
+
+    Runnable task = () -> {
+      spinLock.lock();
+      System.out.println(Thread.currentThread().getName() + "가 락을 획득했습니다.");
+
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      } finally {
+        System.out.println(Thread.currentThread().getName() + "가 락을 해제합니다.");
+        spinLock.unLock();
+      }
+    };
+
+    Thread thread = new Thread(task);
+    Thread thread2 = new Thread(task);
+
+    thread.start();
+    thread2.start();
+  }
+}
