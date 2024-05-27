adc1c7cf7079cc4f817592c22ec65b8c74c13792
limwoobin <drogba02@naver.com>
2024-05-27 19:50:15 +0900

    feat: Java Concurrency Project Section09 Atomic

---
+++ java-concurrency/src/main/java/org/example/section_09/exam02/AtomicIntegerExample.java
@@ -0,0 +1,32 @@
+package org.example.section_09.exam02;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerExample {
+  private static AtomicInteger counter = new AtomicInteger(0);
+  private static int NUM_THREADS = 5;
+  private static int NUM_INCREMENTS = 100000;
+
+  public static void main(String[] args) {
+
+    Thread[] threads = new Thread[NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+      threads[i] = new Thread(() -> {
+        for (int j = 0; j < NUM_INCREMENTS; j++) {
+          counter.incrementAndGet();
+          System.out.println("Counter: " + counter.get());
+        }
+      });
+      threads[i].start();
+    }
+    for (Thread thread : threads) {
+      try {
+        thread.join();
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    System.out.println("Final value: " + counter.get());
+  }
+}
---
+++ java-concurrency/src/main/java/org/example/section_09/exam02/AtomicReferenceExample.java
@@ -0,0 +1,72 @@
+package org.example.section_09.exam02;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class AtomicReferenceExample {
+  public static void main(String[] args) {
+
+    User user1 = new User("Alice", 25);
+    User user2 = new User("Bob", 30);
+
+    AtomicReference<User> atomicReference = new AtomicReference<>(user2);
+
+    Thread thread1 = new Thread(() -> {
+      User updateUser = new User("Carol", 40);
+      boolean success = atomicReference.compareAndSet(user1, updateUser);
+      if (success) {
+        System.out.println("스레드 1 이 " + updateUser + " 로 변경에 성공했습니다.");
+      } else {
+        System.out.println("스레드 1 이 " + updateUser + " 로 변경에 실패했습니다.");
+      }
+    });
+
+    Thread thread2 = new Thread(() -> {
+      User updateUser = new User("David", 50);
+      boolean success = atomicReference.compareAndSet(user2, updateUser);
+      if (success) {
+        System.out.println("스레드 2 가 " + updateUser + " 로 변경에 성공했습니다.");
+      } else {
+        System.out.println("스레드 2 가 " + updateUser + " 로 변경에 실패했습니다.");
+      }
+    });
+
+    thread2.start();
+    thread1.start();
+
+    try {
+      thread1.join();
+      thread2.join();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+
+    System.out.println("Final User: " + atomicReference.get());
+
+  }
+}
+
+class User {
+  private String name;
+  private int age;
+
+  public User(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+      "name='" + name + '\'' +
+      ", age=" + age +
+      '}';
+  }
+}
