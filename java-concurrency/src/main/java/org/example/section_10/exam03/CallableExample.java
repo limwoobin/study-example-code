81e746b97f972557531464fcb5553e167a1df8be
limwoobin <drogba02@naver.com>
2024-06-01 17:48:29 +0900

    feat: Java Concurrency Project Section10 Callable

--- java-concurrency/src/main/java/org/example/section_10/exam03/CallableExample.java
+++ java-concurrency/src/main/java/org/example/section_10/exam03/CallableExample.java
@@ -3,7 +3,7 @@ package org.example.section_10.exam03;
 import java.util.concurrent.*;
 
 public class CallableExample {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     ExecutorService executorService = Executors.newFixedThreadPool(1);
     Callable<Integer> callableTask = () -> {
       System.out.println(Thread.currentThread().getName() + " Callable 작업 수행중..");
@@ -12,6 +12,7 @@ public class CallableExample {
     };
 
     Future<Integer> future = executorService.submit(callableTask);
+    Thread.sleep(3000);
     int result;
     try {
       result = future.get();
---
+++ java-concurrency/src/main/java/org/example/section_10/exam03/CallableExampleV2.java
@@ -0,0 +1,27 @@
+package org.example.section_10.exam03;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.*;
+
+public class CallableExampleV2 {
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    ExecutorService executorService = Executors.newFixedThreadPool(1);
+    Callable<Integer> callableTask = () -> {
+      System.out.println("zz: " + LocalDateTime.now());
+      Thread.sleep(3000);
+      System.out.println("zz: " + LocalDateTime.now());
+      System.out.println(Thread.currentThread().getName() + " Callable 작업 완료");
+      return 42;
+    };
+
+    System.out.println("start");
+    System.out.println("ww: " + LocalDateTime.now());
+    Future<Integer> future = executorService.submit(callableTask);
+    System.out.println("ww: " + LocalDateTime.now());
+    System.out.println("get");
+    int result = future.get();
+    System.out.println("end: " + result);
+
+    executorService.shutdown();
+  }
+}
