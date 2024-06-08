8262cb37596483182080124a1441f0c8de9844ec
limwoobin <drogba02@naver.com>
2024-06-08 21:28:39 +0900

    feat: Java Concurrency Project Section11 ThreadPoolExecutor

---
+++ java-concurrency/src/main/java/org/example/section_11/KeepAliveTimeExample.java
@@ -0,0 +1,42 @@
+package org.example.section_11;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class KeepAliveTimeExample {
+  public static void main(String[] args) throws InterruptedException {
+    int corePoolSize = 2;
+    int maxPoolSize = 4;
+    long keepAliveTime = 1L;
+    BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(2);
+    int taskNum = 6;
+
+    ThreadPoolExecutor executor = new ThreadPoolExecutor(
+      corePoolSize,
+      maxPoolSize,
+      keepAliveTime,
+      TimeUnit.SECONDS,
+      workQueue
+    );
+
+    for (int i = 0; i < taskNum; i++) {
+      final int taskId = i;
+      executor.execute(() -> {
+        try {
+          Thread.sleep(2000);
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+
+        System.out.println(Thread.currentThread().getName() + " 가 태스트" + taskId + " 를 실행");
+      });
+    }
+
+//    executor.allowCoreThreadTimeOut(true);
+
+    Thread.sleep(4000);
+    executor.shutdown();
+  }
+}
---
+++ java-concurrency/src/main/java/org/example/section_11/LinkedBlockingQueueExample.java
@@ -0,0 +1,46 @@
+package org.example.section_11;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class LinkedBlockingQueueExample {
+  public static void main(String[] args) {
+    LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<>(5); // 큐의 최대 용량은 5
+
+    // 생산자 스레드
+    Thread producer = new Thread(() -> {
+      try {
+        for (int i = 1; i <= 10; i++) {
+          System.out.println("Producing: " + i);
+          queue.put(i); // 데이터를 큐에 넣음 (큐가 가득 차면 블록됨)
+          Thread.sleep(10000); // 생산자 스레드는 10초마다 데이터를 생산
+        }
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    });
+
+    // 소비자 스레드
+    Thread consumer = new Thread(() -> {
+      try {
+        for (int i = 1; i <= 10; i++) {
+          int data = queue.take(); // 데이터를 큐에서 가져옴 (큐가 비어있으면 블록됨)
+          System.out.println("Consuming: " + data);
+          Thread.sleep(100); // 소비자 스레드는 0.1초마다 데이터를 소비
+        }
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    });
+
+    producer.start();
+    consumer.start();
+
+    try {
+      producer.join();
+      consumer.join();
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+
+  }
+}
