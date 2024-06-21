1394dcf6d9dc35bbe31d6155af5652ed1db0d919
limwoobin <drogba02@naver.com>
2024-06-21 21:15:21 +0900

    feat: Java Concurrency Project Section12 Combine V2

--- java-concurrency/src/main/java/org/example/section_12/exam07/ThenCombineExample.java
+++ java-concurrency/src/main/java/org/example/section_12/exam07/ThenCombineExample.java
@@ -9,8 +9,10 @@ public class ThenCombineExample {
     CompletableFuture<String> cf1 = service.getData();
     CompletableFuture<String> cf2 = service.getData2();
 
-    CompletableFuture<String> cf3 = cf1.thenCombine(cf2, (value1, value2) -> value1 + value2 + " Java");
-    System.out.println(cf3.join());
+    CompletableFuture<String> cf3 = cf1.thenCombine(cf2, (value1, value2) -> value1 + value2);
+    CompletableFuture<String> cf4 = cf3.thenCompose(value -> CompletableFuture.supplyAsync(() -> value + " Java"));
+
+    System.out.println(cf4.join());
   }
 
   static class MyService {
