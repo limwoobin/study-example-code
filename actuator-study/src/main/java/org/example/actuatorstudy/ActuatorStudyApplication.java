db189183db1cb70ce58ebdd8f516dfea6ea445ec
limwoobin <drogba02@naver.com>
2024-12-22 18:19:52 +0900

    feat: actuator project init

---
+++ actuator-study/src/main/java/org/example/actuatorstudy/ActuatorStudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.actuatorstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorStudyApplication.class, args);
+	}
+
+}
---
+++ actuator-study/src/test/java/org/example/actuatorstudy/ActuatorStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.actuatorstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
